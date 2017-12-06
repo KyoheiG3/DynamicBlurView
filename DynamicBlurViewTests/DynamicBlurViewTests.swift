bfee3ea03a60dd4e1c3214e6b842e0a59065c0b5
Tóth Balázs <toth.balazs@me.com>
2017-12-06 16:32:48 +0100

    Fixes an EXC_BAD_ACCESS error in CGImage+Accelerate
    - Adds a sample image to test on
    - Adds a test to confirm the image is getting blurred as well

--- DynamicBlurViewTests/DynamicBlurViewTests.swift
+++ DynamicBlurViewTests/DynamicBlurViewTests.swift
@@ -8,29 +8,26 @@
 
 import UIKit
 import XCTest
+@testable import DynamicBlurView
 
 class DynamicBlurViewTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  func testBlurring() {
+    let bundle = Bundle(for: type(of: self))
+    let path = bundle.path(forResource: "cat", ofType: "jpg")
+    guard let image = UIImage(contentsOfFile: path!) else {
+      return XCTFail("Test image not found")
+    }
+
+    guard let blurredImage = image.blurred(radius: 17,
+                                           iterations: 3,
+                                           ratio: 1,
+                                           blendColor: nil,
+                                           blendMode: CGBlendMode.clear) else {
+                                            return XCTFail("Blur method failed")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    XCTAssertNotEqual(image, blurredImage)
+  }
+  
 }
